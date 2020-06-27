9663e50a0b18eb50ead7279737f2f2651e2ff265
LAPTOP-03186LR8\berry <a01056239778@gmail.com>
2020-06-27 21:51:52 +0900

    BubbleSortTest 클래스 객체 한 번만 생성하도록 변경

--- test/array/BubbleSortTest.java
+++ test/array/BubbleSortTest.java
@@ -6,35 +6,32 @@ import static org.junit.Assert.assertArrayEquals;
 
 
 public class BubbleSortTest {
+    BubbleSort bubbleSort = new BubbleSort();
 
     @Test
-    public void sortTest1(){
+    public void sortTest1() {
         int[] array = {1, 2, 3};
-        BubbleSort bubbleSort = new BubbleSort();
         int[] result = bubbleSort.sort(array);
         assertArrayEquals(result, new int[]{1, 2, 3});
     }
 
     @Test
-    public void sortTest2(){
+    public void sortTest2() {
         int[] array = {3, 2, 1};
-        BubbleSort bubbleSort = new BubbleSort();
         int[] result = bubbleSort.sort(array);
         assertArrayEquals(result, new int[]{1, 2, 3});
     }
 
     @Test
-    public void sortTest3(){
+    public void sortTest3() {
         int[] array = {1};
-        BubbleSort bubbleSort = new BubbleSort();
         int[] result = bubbleSort.sort(array);
         assertArrayEquals(result, new int[]{1});
     }
 
     @Test
-    public void sortTest4(){
+    public void sortTest4() {
         int[] array = {1, 5, 3, 3, 2, 7, 8, 9, 10, 2};
-        BubbleSort bubbleSort = new BubbleSort();
         int[] result = bubbleSort.sort(array);
         assertArrayEquals(result, new int[]{1, 2, 2, 3, 3, 5, 7, 8, 9, 10});
     }
