182bfdef8971e075fd2c84e9184d1e982c7f49ee
LAPTOP-03186LR8\berry <a01056239778@gmail.com>
2020-06-30 23:09:32 +0900

    합집합 교집합 추가

--- src/array/IntersectionUnion.java
+++ src/array/IntersectionUnion.java
@@ -1,39 +1,42 @@
 package array;
 
+import java.util.HashSet;
+
 public class IntersectionUnion {
-    public static void main(String[] args) {
-        int[] a = {1, 2, 3};
-        int[] b = {2, 3, 4};
-        int[] Intersection = new int[a.length + b.length];
+    public int[] intersection(int[] a, int[] b) {
+        HashSet<Integer> A = new HashSet();
+        HashSet<Integer> B = new HashSet();
         for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < b.length; j++) {
-                if (a[i] == b[j]) {
-                    Intersection[i] = a[i];
-
-                }
-            }
-        }
-        for (int i = 0; i < Intersection.length; i++) {
-            System.out.print(Intersection[i]);
-        }
-        System.out.println();
-
-        int[] union = new int[a.length + b.length];
-        for (int i = 0; i < a.length; i++) {
-            Intersection[i] = a[i];
+            A.add(a[i]);
         }
         for (int i = 0; i < b.length; i++) {
-            Intersection[a.length + i] = b[i];
+            B.add(b[i]);
         }
-        for (int i = 0; i < Intersection.length; i++) {
-            for (int j = i + 1; j < Intersection.length; j++) {
-                if (Intersection[i] == Intersection[j]) {
-                    Intersection[j] = 0;
-                }
-            }
+        A.retainAll(B);
+        Integer[] result = new Integer[A.size()];
+        A.toArray(result);
+        // a, b의 교집합, 오름차순 정렬해서 반환할 것
+
+        //return result;
+        return null;
+    }
+
+    public int[] union(int[] a, int[] b) {
+        HashSet<Integer> A = new HashSet();
+        HashSet<Integer> B = new HashSet();
+        for (int i = 0; i < a.length; i++) {
+            A.add(a[i]);
         }
-        for (int i = 0; i < Intersection.length; i++) {
-            System.out.print(Intersection[i]);
+        for (int i = 0; i < b.length; i++) {
+            B.add(b[i]);
         }
+        A.addAll(B);
+        Integer[] result = new Integer[A.size()];
+        int[] unionResult = new int[A.size()];
+        A.toArray(result);
+        // a, b의 교집합, 오름차순 정렬해서 반환할 것
+        //return result;
+        // a, b의 합집합, 오름차순 정렬해서 반환할 것
+        return null;
     }
 }
