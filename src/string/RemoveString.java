d2b3ac1b79bf4615f19dc495422d02c9ae10a869
LAPTOP-03186LR8\berry <a01056239778@gmail.com>
2020-07-02 22:32:51 +0900

    string remove 추가

--- src/string/RemoveString.java
+++ src/string/RemoveString.java
@@ -2,7 +2,13 @@ package string;
 
 public class RemoveString {
     public String remove(String s){
+        String result="";
+        if(s.length()<3){
+            result="";
+        }else{
+            result=s.substring(1,s.length()-1);
+        }
         // 입력된 문자열 s의 첫 글자와 마지막 글자를 제거한 후 반환한다.        
-        return null;
+        return result;
     }
 }
--- test/string/RemoveStringTest.java
+++ test/string/RemoveStringTest.java
@@ -11,34 +11,34 @@ public class RemoveStringTest {
     public void removeTest1() {
         String s = "abcde";
         String result = removeString.remove(s);
-        assertEquals("bcd", s);
+        assertEquals("bcd", result);
     }
 
     @Test
     public void removeTest2() {
         String s = "ab";
         String result = removeString.remove(s);
-        assertEquals("", s);
+        assertEquals("", result);
     }
 
     @Test
     public void removeTest3() {
         String s = "12345";
         String result = removeString.remove(s);
-        assertEquals("234", s);
+        assertEquals("234", result);
     }
 
     @Test
     public void removeTest4() {
         String s = "a1b2c3d4e5";
         String result = removeString.remove(s);
-        assertEquals("1b2c3d4e", s);
+        assertEquals("1b2c3d4e", result);
     }
 
     @Test
     public void removeTest5() {
         String s = "aAbBcCdDeE";
         String result = removeString.remove(s);
-        assertEquals("AbBcCdDe", s);
+        assertEquals("AbBcCdDe", result);
     }
 }
