0490ef49367d8db6b2e1942ff18ec23f6bc05955
LAPTOP-03186LR8\berry <a01056239778@gmail.com>
2020-07-02 22:06:30 +0900

    substring 이용한 문제풀이

--- src/string/MaskingString.java
+++ src/string/MaskingString.java
@@ -1,8 +1,20 @@
 package string;
 
 public class MaskingString {
-    public String masking(String s){
+    public String masking(String s) {
+        String result = s;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'A' || s.charAt(i) == 'a') {
+                if (i == 0) {
+                    result = '*' + result.substring(1);
+                } else if (i == s.length() - 1) {
+                    result = result.substring(0, i) + '*';
+                } else {
+                    result = result.substring(0, i ) + '*' + result.substring(i + 1);
+                }
+            }
+        }
         // 입력된 문자열 s의 'A'와 'a' 문자를 '*'로 변경하여 반환한다.
-        return null;
+        return result;
     }
 }
