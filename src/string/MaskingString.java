92069a21b29650ae894a08c74c21b3712f1ebaa3
LAPTOP-03186LR8\berry <a01056239778@gmail.com>
2020-07-02 22:12:06 +0900

    charAt 사용한 문제풀이

--- src/string/MaskingString.java
+++ src/string/MaskingString.java
@@ -2,6 +2,7 @@ package string;
 
 public class MaskingString {
     public String masking(String s) {
+        /*
         String result = s;
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == 'A' || s.charAt(i) == 'a') {
@@ -16,5 +17,16 @@ public class MaskingString {
         }
         // 입력된 문자열 s의 'A'와 'a' 문자를 '*'로 변경하여 반환한다.
         return result;
+
+         */
+        String result2="";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'A' || s.charAt(i) == 'a') {
+                result2 = result2 + '*';
+            } else {
+                result2 = result2 + s.charAt(i);
+            }
+        }
+        return result2;
     }
 }
