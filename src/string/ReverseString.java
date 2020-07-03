0cca4b96e85ac61779ca79f52291d5c902e12f95
LAPTOP-03186LR8\berry <a01056239778@gmail.com>
2020-07-03 19:20:37 +0900

    reverseString 추가

--- src/string/ReverseString.java
+++ src/string/ReverseString.java
@@ -1,8 +1,12 @@
 package string;
 
 public class ReverseString {
-    public String reverse(String s){
+    public String reverse(String s) {
+        String result = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            result = result + s.charAt(i);
+        }
         // 입력된 문자열 s를 거꾸로 변환하여 반환한다.
-        return null;
+        return result;
     }
 }
--- test/string/ReverseStringTest.java
+++ test/string/ReverseStringTest.java
@@ -11,34 +11,34 @@ public class ReverseStringTest {
     public void reverseTest1() {
         String s = "abcde";
         String result = reverseString.reverse(s);
-        assertEquals("edcba", s);
+        assertEquals("edcba", result);
     }
 
     @Test
     public void reverseTest2() {
         String s = "aAbBcCdDeE";
         String result = reverseString.reverse(s);
-        assertEquals("EeDdCcBbAa", s);
+        assertEquals("EeDdCcBbAa", result);
     }
 
     @Test
     public void reverseTest3() {
         String s = "a1b2c3d4e5";
         String result = reverseString.reverse(s);
-        assertEquals("5e4d3c2b1a", s);
+        assertEquals("5e4d3c2b1a", result);
     }
 
     @Test
     public void reverseTest4() {
-        String s = "$#@!edbca";
+        String s = "$#@!edcba";
         String result = reverseString.reverse(s);
-        assertEquals("abcde!@#$", s);
+        assertEquals("abcde!@#$", result);
     }
 
     @Test
     public void reverseTest5() {
         String s = "";
         String result = reverseString.reverse(s);
-        assertEquals("", s);
+        assertEquals("", result);
     }
 }
