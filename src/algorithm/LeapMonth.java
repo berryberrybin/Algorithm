0789a21bae7ffcb512a5ae723a3273598801cdb9
LAPTOP-03186LR8\berry <a01056239778@gmail.com>
2020-07-08 22:57:33 +0900

    LeapMonth 배열을 이용하여 정리

--- src/algorithm/LeapMonth.java
+++ src/algorithm/LeapMonth.java
@@ -2,19 +2,15 @@ package algorithm;
 
 public class LeapMonth {
     public String solution(int a, int b) {
+        int[] daysOfMonth = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         int date = 0;
-        for(int i=1;i<a;i++){
-            if(i==1||i==3||i==5||i==7||i==8||i==10||i==12){
-                date = date + 31;
-            } else if(i==4||i==6||i==9||i==11){
-                date = date + 30;
-            } else{
-                date = date + 29;
-            }
+        for (int i = 0; i < a - 1; i++) {
+            date = date + daysOfMonth[i];
         }
+
         date = date + b;
-        int remainder = date%7;
-        String[]week={"THU","FRI","SAT","SUN","MON","TUE","WED"};
+        int remainder = date % 7;
+        String[] week = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
 
         String answer = week[remainder];
         return answer;
@@ -22,7 +18,7 @@ public class LeapMonth {
 
     public static void main(String[] args) {
         LeapMonth leapMonth = new LeapMonth();
-        String result = leapMonth.solution(5,24);
+        String result = leapMonth.solution(5, 24);
         System.out.println(result);
     }
 }
