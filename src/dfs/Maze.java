3bb810512b001038791c1f85fe640d206caa7023
LAPTOP-03186LR8\berry <a01056239778@gmail.com>
2020-07-11 22:36:50 +0900

    Maze 실시간 출력 추가

--- src/dfs/Maze.java
+++ src/dfs/Maze.java
@@ -8,29 +8,50 @@ public class Maze {
     public Maze(int[][] map) {
         this.map = map;
         visited = new boolean[map.length][map.length];
+
     }
 
     public void solution(int i, int j) {
         // 시작점: (0, 0)
         // 종료점: (n-1, n-1)
         visited[i][j] = true;
-        System.out.println("( " + i + " , " + j + " )");
-        if(i==map.length-1&&j==map.length-1){
-            find=true;
+        printMaze(i, j);
+        if (i == map.length - 1 && j == map.length - 1) {
+            find = true;
             System.out.println("찾았다.");
             return;
         }
-        if (j < map.length - 1 && map[i][j + 1] == 0 && visited[i][j+1] == false&&find==false) {
+        if (j < map.length - 1 && map[i][j + 1] == 0 && visited[i][j + 1] == false && find == false) {
             solution(i, j + 1);
         }
-        if (i < map.length - 1 && map[i + 1][j] == 0 && visited[i+1][j] == false&&find==false) {
+        if (i < map.length - 1 && map[i + 1][j] == 0 && visited[i + 1][j] == false && find == false) {
             solution(i + 1, j);
         }
-        if (i > 0 && map[i - 1][j] == 0 && visited[i-1][j] == false&&find==false) {
+        if (i > 0 && map[i - 1][j] == 0 && visited[i - 1][j] == false && find == false) {
             solution(i - 1, j);
         }
-        if (j > 0 && map[i][j - 1] == 0 && visited[i][j-1] == false&&find==false) {
+        if (j > 0 && map[i][j - 1] == 0 && visited[i][j - 1] == false && find == false) {
             solution(i, j - 1);
+        }
+    }
+
+    public void printMaze(int x, int y) {
+        try {
+            Thread.sleep(100);
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        } catch (Exception e) {
+        }
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map.length; j++) {
+                if (i == x && j == y) {
+                    System.out.print('★');
+                } else if (map[i][j] == 0) {
+                    System.out.print('□');
+                } else {
+                    System.out.print('■');
+                }
+            }
+            System.out.println();
         }
     }
 
