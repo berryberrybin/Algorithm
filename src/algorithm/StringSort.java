9f5d82ce5ef6ff7948cb87057443364adb66c179
LAPTOP-03186LR8\berry <a01056239778@gmail.com>
2020-07-11 12:18:02 +0900

    string Sort 알고리즘 수정

--- src/algorithm/StringSort.java
+++ src/algorithm/StringSort.java
@@ -1,40 +1,35 @@
 package algorithm;
 
-import java.util.ArrayList;
 
 public class StringSort {
 
     public String[] solution(String[] strings, int n) {
-        char[] kthChar = new char[strings.length];
-        for(int i=0;i< strings.length;i++){
-            kthChar[i] =strings[i].charAt(n);
-        }
-        int[] kthCharToInt = new int[strings.length];
-        for(int i=0;i<kthChar.length;i++){
-            kthCharToInt[i]=(int)kthChar[i];
-        }
-        ArrayList<String> stringsSort = new ArrayList<>();
-        for(int i=97;i<=122;i++){
-            for(int j=0;j<kthCharToInt.length;j++){
-                if(i==kthChar[j]){
-                    stringsSort.add(strings[j]);
+        int minLocation = 0;
+        for (int i = 0; i < strings.length - 1; i++) {
+            minLocation = i;
+            for (int j = i; j < strings.length; j++) {
+                if (strings[minLocation].charAt(n) > strings[j].charAt(n)) {
+                    minLocation = j;
+                } else if(strings[minLocation].charAt(n) == strings[j].charAt(n)){
+                    if(strings[minLocation].compareTo(strings[j])>0){
+                        minLocation = j;
+                    }
                 }
             }
+            String minString=strings[minLocation];
+            strings[minLocation] = strings[i];
+            strings[i] = minString;
         }
-        String[] answer= new String[strings.length];
-
-        for(int i=0;i<stringsSort.size();i++){
-            answer[i]= stringsSort.get(i);
-        }
-        return answer;
+        return strings;
     }
 
     public static void main(String[] args) {
         StringSort stringsort = new StringSort();
-        String [] strings = {"sun", "bed", "car"};
-        String[] result = stringsort.solution(strings,1);
-        for(int i=0;i<result.length;i++) {
+        String[] strings = {"sun", "bed", "car","bar"};
+        String[] result = stringsort.solution(strings, 1);
+        for (int i = 0; i < result.length; i++) {
             System.out.println(result[i]);
         }
     }
 }
+// kthCharToInt가 같을때 재정렬 필요.
---
+++ src/inheritance/Animal.java
@@ -0,0 +1,5 @@
+package inheritance;
+
+public class InheritanceBasic {
+
+}
