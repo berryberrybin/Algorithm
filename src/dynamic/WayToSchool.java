107a69a77229585ad83b5e24840390ff5984f5a0
LAPTOP-03186LR8\berry <a01056239778@gmail.com>
2020-07-18 17:00:23 +0900

    학교가는길 알고리즘 코드 수정

--- src/dynamic/WayToSchool.java
+++ src/dynamic/WayToSchool.java
@@ -10,39 +10,45 @@ public class WayToSchool {
         }
         way[0][0] = 1;
         for (int i = 0; i < puddles.length; i++) {
-            way[(puddles[i][0]) - 1][(puddles[i][1]) - 1] = 0;
+            way[(puddles[i][1]) - 1][(puddles[i][0]) - 1] = 0;
         }
         for (int i = 0; i < n; i++) {
-            if (way[i][0] != 0) {
-                way[i][0] = 1;
-            } else if (way[i][0] == 0){
+            if (way[i][0] == 0) {
+                for (int k = i; k < n; k++) {
+                    way[k][0] = 0;
+                }
                 break;
+            } else {
+                way[i][0] = 1;
             }
         }
         for (int i = 0; i < m; i++) {
-            if (way[0][i] != 0) {
-                way[0][i] = 1;
-
-            } else if(way[0][i] != 0){
+            if (way[0][i] == 0) {
+                for (int k = i; k < m; k++) {
+                    way[0][k] = 0;
+                }
                 break;
+            } else {
+                way[0][i] = 1;
             }
         }
         for (int i = 1; i < n; i++) {
             for (int j = 1; j < m; j++) {
                 if (way[i][j] == -1) {
-                    way[i][j] = way[i][j - 1] % 1000000007 + way[i - 1][j] % 1000000007;
+                    way[i][j] = way[i][j - 1] + way[i - 1][j];
                     way[i][j] = way[i][j] % 1000000007;
                 }
             }
         }
-        long answer = way[n - 1][m - 1] % 1000000007;
+        long answer = way[n - 1][m - 1];
         return (int) answer;
     }
 
     public static void main(String[] args) {
         WayToSchool waytoschool = new WayToSchool();
-        int result = waytoschool.solution(2, 2, new int[][]{{2, 1}});
+        int result = waytoschool.solution(3, 4, new int[][]{{1, 2}, {2, 4}});
         System.out.println(result);
     }
 }
 
+//https://programmers.co.kr/learn/courses/30/lessons/42898
