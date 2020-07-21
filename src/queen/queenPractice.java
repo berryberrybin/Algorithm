5d8ca45541a7c05ce16f40e7ea063474f2d0f753
LAPTOP-03186LR8\berry <a01056239778@gmail.com>
2020-07-21 23:08:00 +0900

    퀸 알고리즘 수정

--- src/queen/queenPractice.java
+++ src/queen/queenPractice.java
@@ -1,6 +1,7 @@
 package queen;
 
 public class queenPractice {
+    static int count =0;
     static boolean[] flag_a = new boolean[8];
     static boolean[] flag_b = new boolean[15];
     static boolean[] flag_c = new boolean[15];
@@ -9,26 +10,28 @@ public class queenPractice {
     static void print() {
         for (int i = 0; i < 8; i++) {
             System.out.printf("%2d", pos[i]);
-            System.out.println();
         }
+        count++;
+        System.out.println();
     }
 
     static void set(int i) {
-        for (int j = 0; j < 0; j++) {
+        for (int j = 0; j < 8; j++) {
             if (flag_a[j] == false && flag_b[i + j] == false && flag_c[i - j + 7] == false) {
                 pos[i] = j;
                 if (i == 7) {
                     print();
+                } else {
+                    flag_a[j] = flag_b[i + j] = flag_c[i - j + 7] = true;
+                    set(i + 1);
+                    flag_a[j] = flag_b[i + j] = flag_c[i - j + 7] = false;
                 }
-            } else {
-                flag_a[j] = flag_a[i + j] = flag_c[i - j + 7] = true;
-                set(i + 1);
-                flag_a[j] = flag_a[i + j] = flag_c[i - j + 7] = false;
             }
         }
     }
 
     public static void main(String[] args) {
         set(0);
+        System.out.println(count);
     }
 }
