cfe3314615be5d9d4458ba943807197225d1abe4
LAPTOP-03186LR8\berry <a01056239778@gmail.com>
2020-07-26 13:03:02 +0900

    [#4]  가장 먼 노드 클래스 생성

---
+++ src/algorithm/LongestNode.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+public class LongestNode {
+    public int solution(int n, int[][] edge) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        LongestNode longestnode = new LongestNode();
+        int[][] edge = {{3, 6}, {4, 3}, {3, 2}, {1, 3}, {1, 2}, {2, 4}, {5, 2}};
+        int n =6;
+        System.out.println(longestnode.solution(n, edge));
+    }
+
+}
+// https://programmers.co.kr/learn/courses/30/lessons/49189
