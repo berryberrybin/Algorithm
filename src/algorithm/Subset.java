0e10c0bbf2ab0a6fae3edd31ba79527abc27639c
LAPTOP-03186LR8\berry <a01056239778@gmail.com>
2020-07-28 14:50:35 +0900

    부분집합 알고리즘 수정

--- src/algorithm/Subset.java
+++ src/algorithm/Subset.java
@@ -1,4 +1,42 @@
 package algorithm;
 
 public class Subset {
+    int n;
+    boolean[] check;
+
+    public void solution(int n) {
+        check = new boolean[n];
+        this.n = n;
+        check(0);
+    }
+
+    private void check(int i) {
+        if (i == n) {
+            printCheck();
+            System.out.println();
+            return;
+        }
+        check[i] = false;
+        check(i + 1);
+        check[i] = true;
+        check(i + 1);
+    }
+
+    private void printCheck() {
+        System.out.print("{");
+        for (int i = 0; i < check.length; i++) {
+            if (check[i]) {
+                System.out.print(i + 1);
+            } else {
+                continue;
+            }
+        }
+        System.out.print("}");
+    }
+
+    public static void main(String[] args) {
+        Subset subset = new Subset();
+        int n = 4;
+        subset.solution(n);
+    }
 }
