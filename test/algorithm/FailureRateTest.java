d5716598f41ae0ddcb0aa2f94fe13d746823286f
문수빈 <a01056239778@gmail.com>
2021-12-11 13:24:55 +0900

    코드 클래스명 대문자로 변경

--- test/algorithm/FailureRateTest.java
+++ test/algorithm/FailureRateTest.java
@@ -4,12 +4,12 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertArrayEquals;
 
-public class failureRateTest {
+public class FailureRateTest {
     @Test
     public void failureRateTest1() {
         int N = 5;
         int[] stages = {2, 1, 2, 6, 2, 4, 3, 3};
-        failureRate failurerate = new failureRate();
+        FailureRate failurerate = new FailureRate();
         int[] result = failurerate.solution(N, stages);
         assertArrayEquals(new int[]{3, 4, 2, 1, 5}, result);
     }
