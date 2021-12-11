9dde2323b757f69d1e054345fdec5ba7ca9d291d
문수빈 <a01056239778@gmail.com>
2021-12-12 00:23:50 +0900

    코드 주석 추가

--- src/array/RotateArray.java
+++ src/array/RotateArray.java
@@ -18,34 +18,34 @@ public class RotateArray {
                 arrayData[i][j] = Integer.parseInt(inputArrayData[k++]); // 문자열로 입력된 배열 값을 정수형태로 변경하여 배열에 저장
             }
         }
-        for (int i = 0; i < rotateOrder.length; i++) {
-            if (rotateOrder[i].equals("R")) {
+        for (int i = 0; i < rotateOrder.length; i++) { // 회전명령어를 차례로 수행
+            if (rotateOrder[i].equals("R")) { // R 입력받은 경우 오른쪽으로 회전
                 int[][] resultRightRotate = new int[arrayData[0].length][arrayData.length];
                 for (int m = 0; m < arrayData.length; m++) {
                     for (int n = 0; n < arrayData[0].length; n++) {
-                        resultRightRotate[n][arrayData.length - 1 - m] = arrayData[m][n];
+                        resultRightRotate[n][arrayData.length - 1 - m] = arrayData[m][n]; // 오른쪽 회전시 인덱스 위치로 변경
                     }
                 }
-                arrayData = resultRightRotate;
-            } else if (rotateOrder[i].equals("L")) {
+                arrayData = resultRightRotate; // arrayData 갱신
+            } else if (rotateOrder[i].equals("L")) { // L 입력받은 경우 왼쪽으로 회전
                 int[][] resultLeftRotate = new int[arrayData[0].length][arrayData.length];
                 for (int m = 0; m < arrayData.length; m++) {
                     for (int n = 0; n < arrayData[0].length; n++) {
-                        resultLeftRotate[n][m] = arrayData[m][n];
+                        resultLeftRotate[n][m] = arrayData[m][n]; // 왼쪽 회전시 인덱스 위치로 변경
                     }
                 }
-                arrayData = resultLeftRotate;
-            } else {
+                arrayData = resultLeftRotate; // arrayData 갱신
+            } else { // R이나 L이아니면 뒤집기 수행
                 int[][] resultReverseRotate = new int[arrayData.length][arrayData[0].length];
                 for (int m = 0; m < arrayData.length; m++) {
                     for (int n = 0; n < arrayData[0].length; n++) {
-                        resultReverseRotate[m][arrayData[0].length - 1 - n] = arrayData[m][n];
+                        resultReverseRotate[m][arrayData[0].length - 1 - n] = arrayData[m][n]; // 뒤집을 경우 인덱스 위치로 변경
                     }
                 }
-                arrayData = resultReverseRotate;
+                arrayData = resultReverseRotate; // arrayData 갱신
             }
         }
-        int answer = arrayData[Integer.parseInt(answerNumberLocation[1]) - 1][Integer.parseInt(answerNumberLocation[0]) - 1];
+        int answer = arrayData[Integer.parseInt(answerNumberLocation[1]) - 1][Integer.parseInt(answerNumberLocation[0]) - 1]; // 입력받은 출력할 배열 위치에 저장된 값
         System.out.println(answer);
     }
 }
--- src/string/TimeInterval.java
+++ src/string/TimeInterval.java
@@ -6,33 +6,33 @@ public class TimeInterval {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] startTime = sc.next().split(":");
-        String[] endTime = sc.next().split(":");
+        String[] startTime = sc.next().split(":"); // 시작시간 :를 기준으로 구분하여 String에 저장
+        String[] endTime = sc.next().split(":"); // 종료시간 :를 기준으로 구분하여 String에 저장
 
-        int startHour = Integer.parseInt(startTime[0]);
-        int startMinute = Integer.parseInt(startTime[1]);
-        int endHour = Integer.parseInt(endTime[0]);
-        int endMinute = Integer.parseInt(endTime[1]);
+        int startHour = Integer.parseInt(startTime[0]); // 시작시간 HH 숫자로 변환
+        int startMinute = Integer.parseInt(startTime[1]); // 시작시간 MM 숫자로 변환
+        int endHour = Integer.parseInt(endTime[0]); // 종료시간 HH 숫자로 변환
+        int endMinute = Integer.parseInt(endTime[1]); // 종료시간 MM 숫자로 변환
 
-        int intervalHour = 0;
-        int intervalMinute = 0;
-        if (endMinute >= startMinute) {
-            intervalMinute = endMinute - startMinute;
-            if (endHour >= startHour) {
-                intervalHour = endHour - startHour;
+        int intervalHour = 0; // HH 차이
+        int intervalMinute = 0; // MM 차이
+        if (endMinute >= startMinute) { // 종료 MM이 시작 MM 보다 크거나 같을때
+            intervalMinute = endMinute - startMinute; // 종료 MM - 시작 MM
+            if (endHour >= startHour) {  // 종료 HH가 시작 HH 보다 크거나 같을때
+                intervalHour = endHour - startHour; // 종료 HH - 시작 HH
             } else {
-                intervalHour = 24 - startHour + endHour;
+                intervalHour = 24 - startHour + endHour; // 종료 HH가 시작 HH 보다 작으면 24 - 시작 HH + 종료 HH 수행
             }
-        } else {
-            intervalMinute = 60 - startMinute + endMinute;
-            if (endHour >= startHour) {
-                intervalHour = endHour - 1 - startHour;
+        } else { // 종료 MM이 시작 MM 보다 작을때
+            intervalMinute = 60 - startMinute + endMinute;  // 60 -  시작 MM + 종료 MM 수행
+            if (endHour >= startHour) { //  종료 HH가 시작 HH 보다 클거나 같을때
+                intervalHour = endHour - 1 - startHour; // 종료 HH - 1 - 시작 HH
 
-            } else {
-                intervalHour = 24 - startHour + endHour;
+            } else { // 종료 HH가 시작 HH 보다 작을때
+                intervalHour = 24 - startHour + endHour; // 24 - 시작HH + 종료HH
             }
         }
-        int answer = (intervalHour * 60) + intervalMinute;
+        int answer = (intervalHour * 60) + intervalMinute; // 분단위 시간 간격 = HH 시간 간격 * 60 + MM 시간 간격
         System.out.println("출력 : " + answer);
         sc.close();
     }
